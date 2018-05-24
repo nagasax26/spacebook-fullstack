2a6413b769583be414688170efc331ad3ef66ce9
nagasax26 <smarts003@gmail.com>
2018-05-25 00:25:27 +0300

    adding comment and removing using mongoose

--- routes/posts.js
+++ routes/posts.js
@@ -3,26 +3,64 @@ var Router = express.Router();
 
 var Post = require('../models/postModel');
 
-Router.get('/', function(req, res){
-    Post.find({}, function(err, posts){
-        if(err) throw err;
+Router.get('/', function (req, res) {
+    Post.find({}, function (err, posts) {
+        if (err) throw err;
         res.send(posts);
     });
 });
 
-Router.post('/', function(req, res){
+Router.post('/', function (req, res) {
     var postText = req.body.postText;
-    var newPost = new Post({text: postText, comments:[]});
-    newPost.save(function(err, post){
-        if(err) throw err;
+    var newPost = new Post({
+        text: postText,
+        comments: []
+    });
+    newPost.save(function (err, post) {
+        if (err) throw err;
         res.send(post);
     });
 });
 
-Router.delete('/:id', function(req, res){
+Router.delete('/:id', function (req, res) {
     var id = req.params.id;
-    Post.remove({_id: id}, function(err, result){
-        if(err) throw err;
+    Post.remove({
+        _id: id
+    }, function (err, result) {
+        if (err) throw err;
+        res.send(result);
+    });
+});
+
+Router.post('/:postId/comments', function (req, res) {
+    var postId = req.params.postId;
+    Post.findOne({
+        _id: postId
+    }).exec().then(function (post) {
+        //we cant push to array and then save because the $pushAll is depcreat in mongoose > v4.x
+        //so we use concat
+        post.comments = post.comments.concat(req.body); 
+        post.save(function (err, post) {
+            res.send(post);
+        });
+    }).catch(function (err) {
+        throw err;
+    });
+});
+
+Router.delete('/:postId/comments/:commentId', function (req, res) {
+    var postId = req.params.postId;
+    var commentId = req.params.commentId;
+    Post.update({
+        _id: postId
+    }, {
+        $pull: {
+            comments: {
+                _id: commentId
+            }
+        }
+    }, function (err, result) {
+        if (err) throw err;
         res.send(result);
     });
 });
